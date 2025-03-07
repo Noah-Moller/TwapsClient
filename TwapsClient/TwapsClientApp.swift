e51e05032b8bbe000733a87944b76cf785dfb19d
Noah Moller <noah@tetrix.tech>
2025-03-07 13:15:59 +1000

    Initial Commit

---
+++ TwapsClient/TwapsClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwapsClientApp.swift
+//  TwapsClient
+//
+//  Created by Noah Moller on 7/3/2025.
+//
+
+import SwiftUI
+
+@main
+struct TwapsClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TwapsClientTests/TwapsClientTests.swift
@@ -0,0 +1,17 @@
+//
+//  TwapsClientTests.swift
+//  TwapsClientTests
+//
+//  Created by Noah Moller on 7/3/2025.
+//
+
+import Testing
+@testable import TwapsClient
+
+struct TwapsClientTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
